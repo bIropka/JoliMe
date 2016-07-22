8f2ed2786f67baccb507fb1a1cedf051ba276b91
Iegor Stetsiura <biropka@gmail.com>
2016-07-22 17:27:04 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -44,4 +44,59 @@ $(document).ready(function () {
 
     });
 
+    $('.tab-controls li').click(function() {
+
+        var currentChoice = $(this).index();
+
+        $('.tab-controls .active').removeClass('active');
+        $(this).toggleClass('active');
+
+        $('.show-all .active').removeClass('active');
+        $('.show-all li').eq(currentChoice).toggleClass('active');
+
+        $('.tab .active').removeClass('active');
+        $('.tab li').eq(currentChoice).toggleClass('active');
+
+    });
+
+    $(function($) {
+        $('#form-mailing').validatr({
+            showall: true
+        });
+    });
+
+    /** scripts for slider **/
+
+    $('.slider').slick({
+
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000
+
+    });
+
+    $('.slider-dots li').click(function() {
+
+        $('.slider').slick('slickGoTo', $(this).index());
+
+    });
+
+    $('.slider').on('afterChange', function(event, slick, currentSlide){
+
+        $('.slider-dots .active').toggleClass('active');
+        $('.slider-dots li').eq(currentSlide).toggleClass('active');
+
+    });
+
+    $('.slider-dots li').hover(
+        function() {
+            $('.slider').slick('slickPause');
+        },
+        function() {
+            $('.slider').slick('slickPlay');
+        }
+    );
+
+    /** end of scripts for slider **/
+
 });
